f96116ef0003fe83170eeb13f939ef967e50b284
prince22ji <prince22ji@users.noreply.github.com>
2020-12-05 22:27:17 +0530

    settled the navbar and photos div

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import './App.css';
 import Navbar from './navbar/Navbar'
-
+import Photos from './photos/Photos'
 function App() {
   return (
     <div className="App">
-      <div className="mainContent">
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-        <h1>for testing</h1>
-      </div>
+      <Photos />
       <Navbar />
     </div>
   );
---
+++ src/photos/photos.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './photos.css'
+function Photos() {
+    return (
+        <div className="photoDiv">
+            <div className="photoGrid">
+            
+            <div style={{background:"white"}}>for test</div>
+            <div style={{background:"white"}}>for test</div>
+            <div style={{background:"white"}}>for test</div>
+            <div style={{background:"white"}}>for test</div>
+            <div style={{background:"white"}}>for test</div>
+            <div style={{background:"white"}}>for test</div>
+            <div style={{background:"white"}}>for test</div>
+            <div style={{background:"white"}}>for test</div>
+            <div style={{background:"white"}}>for test</div>
+            <div style={{background:"white"}}>for test</div>
+            <div style={{background:"white"}}>for test</div>
+            
+        </div>
+        </div>
+    );
+}
+
+export default Photos;
