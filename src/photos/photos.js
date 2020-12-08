70141cfd50344680099c8fd8eda0afacfede52b5
prince22ji <prince22ji@users.noreply.github.com>
2020-12-08 22:12:15 +0530

    settled the images which were going outisde the container or body

--- src/photos/photos.js
+++ src/photos/photos.js
@@ -1,26 +1,36 @@
 import React, { useEffect, useState } from 'react';
 // import axios from 'axios'
-import 
 import axios from '../axios'
 import './photos.css'
 
 function Photos() {
     const [images, setImages]=useState([])
+    
+    function t(pageNumber){
+        var pageNumber;
+        pageNumber=pageNumber+1;
+        return pageNumber
+    }
+    setInterval(t(),3000)
+    
+ 
     useEffect(()=>{
         async function fetchData(){
-            const request= await axios.get("/?client_id=kuNKOj83rk51RKOomonfSSCBRci5oSYNrXEKEPRHJYA",)
+            const request= await axios.get(`?client_id=kuNKOj83rk51RKOomonfSSCBRci5oSYNrXEKEPRHJYA`)
             setImages(request.data)
         }fetchData()
     },[])
+     
+    console.log(t(1))
     console.log(images)
-    const image= images.map((images)=><img src={images.urls?.regular} alt={images?.alt_description} key={images?.id} style={{width:math.random(0,1)*1000}}/>)
+    const image= images.map((images)=><img src={images.urls?.regular} alt={images?.alt_description} key={images?.id} />)
     return (
         <div className="photoDiv">
             <div className="photoGrid">
-            {images}
+            {image}
         </div>
         </div>
     );
-}
+    }
 
 export default Photos;
