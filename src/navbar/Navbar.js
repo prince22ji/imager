c968e873e22a4554ffb521fad61fffc808bbf692
vivek666 <amalhan43@gmail.com>
2021-01-29 21:42:48 +0530

    Router Fixed

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,32 +1,31 @@
 import React from "react";
 import "./navbar.css";
-import { BrowserRouter, Router, Link, Switch, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 function Navbar() {
-  return (
-    <BrowserRouter>
-      <div className="navbar">
-        <div className="logo">
-          <h1>IMAGER</h1>
-        </div>
-        <ul>
-          <Link to="/">
-            <li>Home</li>
-          </Link>
-          <Link to="/gallery">
-            <li>Gallery</li>
-          </Link>
-          <Link to="/top_picks">
-            <li>Top Picks</li>
-          </Link>
-          <Link to="/top_rated">
-            <li>Top Rated</li>
-          </Link>
-          <Link to="/about">
-            <li>About Us</li>
-          </Link>
-        </ul>
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<div className="navbar">
+			<div className="logo">
+				<h1>IMAGER</h1>
+			</div>
+			<ul>
+				<Link to="/">
+					<li>Home</li>
+				</Link>
+				<Link to="/gallery">
+					<li>Gallery</li>
+				</Link>
+				<Link to="/top_picks">
+					<li>Top Picks</li>
+				</Link>
+				<Link to="/top_rated">
+					<li>Top Rated</li>
+				</Link>
+				<Link to="/about">
+					<li>About Us</li>
+				</Link>
+			</ul>
+		</div>
+	);
 }
 export default Navbar;
